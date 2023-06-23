a353a35a4e0332fe9983b5a7c5efec9a2007301a
David Liu <david.s.liu@gmail.com>
2023-06-23 02:28:36 +0000

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello Feedback App</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+};
+
+export default App;
