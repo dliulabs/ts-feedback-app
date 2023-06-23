28201b59936692cf47d4f97bb3c83246a4f21bd2
David Liu <david.s.liu@gmail.com>
2023-06-23 21:28:24 +0000

    using Redux/Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { deleteFeedbackById } from "./features/feedback/FeedbackSlice";
 
 const App: React.FC = () => {
-  const [feedback, setFeedback] = useState<FeedbackItemType[]>(FeedbackData);
-  // const feedback = useAppSelector((state) => state.feedback.feedback);
-  // const dispatch = useAppDispatch();
+  // const [feedback, setFeedback] = useState<FeedbackItemType[]>(FeedbackData);
+  const feedback = useAppSelector((state) => state.feedback.feedback);
+  const dispatch = useAppDispatch();
 
   const deleteFeedback = (id: number) => {
     if (window.confirm(`are you sure you want to delete feedback ${id}?`)) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-      // dispatch(deleteFeedbackById(Number(id)));
+      // setFeedback(feedback.filter((item) => item.id !== id));
+      dispatch(deleteFeedbackById(Number(id)));
     }
   };
 
